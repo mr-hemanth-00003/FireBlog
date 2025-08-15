bc43ab4cc536942fceb5a3ac469e68025a646b7b
Hema <h89398697@gmail.com>
2025-08-15 07:25:30 +0000

    Error: Invalid src prop (https://github.com/anand-babu-0003/TrueValidato

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,18 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
